b59636b6f4c9c0252f1036f14adf3486296d507a
Yuva Sri Sai <nanithota18102004@gmail.com>
2025-04-05 04:00:29 +0530

    removed warning

--- app/components/Home_Components/ResumeLottie.tsx
+++ app/components/Home_Components/ResumeLottie.tsx
@@ -3,14 +3,6 @@ import React from "react";
 import Lottie from "lottie-react";
 import resume_lottie from "../../../public/resume_lottie.json";
 export default function ResumeLottie() {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: resume_lottie,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
   return (
     <div>
       <Lottie animationData={resume_lottie} loop={true} autoplay={true} />
